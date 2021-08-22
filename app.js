b66f9218c4dd823efbe3bc77b40a5a1647292fb2
Julien <julien.a.laforge@gmail.com>
2021-08-22 21:28:08 +0200

    first routes

--- app.js
+++ app.js
@@ -1,19 +1,21 @@
-const http = require('http');
-
 const express = require('express');
 
 const app = express();
 
-app.use((req, res, next) => {
-  console.log('In the middleware.');
-  next();
+app.use(express.urlencoded({extended: true}));
+app.use(express.json());
+
+app.use('/add-product', (req, res, next) => {
+  res.send('<form action="/product" method="POST"><input type="text" name="title"/><button type="submit">Add product</button></form>');
 });
 
-app.use((req, res, next) => {
-  console.log('In another middleware.');
+app.post('/product', (req, res, next) => {
+  console.log(req.body);
+  res.redirect('/');
+});
+
+app.use('/', (req, res, next) => {
   res.send('<h1>Hello, friend.</h1>');
-})
+});
 
-const server = http.createServer(app);
-
-server.listen(3000);
+app.listen(3000);
