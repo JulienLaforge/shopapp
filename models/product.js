9c1b0d354f5f0e642c7ab94cc62f5408dcfd458d
Julien <julien.a.laforge@gmail.com>
2021-08-23 20:10:28 +0200

    implement MVC

---
+++ models/product.js
@@ -0,0 +1,30 @@
+const fs = require('fs');
+const path = require('path');
+
+const p = path.join(path.dirname(require.main.filename), 'data', 'products.json');
+
+const getProductsFromFiles = cb => {
+  fs.readFile(p, (err, fileContent) => {
+    if (err) cb([])
+    else cb(JSON.parse(fileContent));
+  });
+}
+
+module.exports = class Product {
+  constructor(title) {
+    this.title = title;
+  }
+
+  save() {
+    getProductsFromFiles(products => {
+      products.push(this);
+      fs.writeFile(p, JSON.stringify(products), err => {
+        console.log(err);
+      });
+    });
+  }
+
+  static fetchAll(cb) {
+    getProductsFromFiles(cb);
+  }
+}
