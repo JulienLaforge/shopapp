e1553afe1d8538d583615b51de4ae5af69d21cc2
Julien <julien.a.laforge@gmail.com>
2021-08-26 17:03:21 +0200

    fix delete product bug

--- models/cart.js
+++ models/cart.js
@@ -47,6 +47,9 @@ module.exports = class Cart {
       }
       const updatedCart = { ...JSON.parse(fileContent) };
       const product = updatedCart.products.find(p => p.id === id);
+      if (!product) {
+        return;
+      }
       const productQty = product.qty;
       updatedCart.products = updatedCart.products.filter(p => p.id !==id);
       updatedCart.totalPrice -= productPrice * productQty;
