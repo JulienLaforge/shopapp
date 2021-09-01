14e667a78854c1a00f50a58df3d9cb55507b9c86
Julien <julien.a.laforge@gmail.com>
2021-09-01 18:22:09 +0200

    add host to conf

--- utils/database.js
+++ utils/database.js
@@ -1,16 +1,13 @@
 const Sequelize = require('sequelize');
 
-console.log("****************");
-console.log("****************");
-console.log(process.env);
-console.log("****************");
-console.log("****************");
 const sequelize = new Sequelize(
   process.env.MYSQL_DATABASE,
   process.env.MYSQL_USER,
   process.env.MYSQL_PASSWORD,
   { 
-    dialect: 'mysql'
+    dialect: 'mysql',
+    host: process.env.MYSQL_HOST,
+    port: 3306
   });
 
 module.exports = sequelize;
