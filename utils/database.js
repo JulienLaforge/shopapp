3bd10753a72eb912f005bea9c976222eb8ca18b8
Julien <julien.a.laforge@gmail.com>
2021-09-01 18:07:44 +0200

    see conf

--- utils/database.js
+++ utils/database.js
@@ -1,5 +1,10 @@
 const Sequelize = require('sequelize');
 
+console.log("****************");
+console.log("****************");
+console.log(process.env);
+console.log("****************");
+console.log("****************");
 const sequelize = new Sequelize(
   process.env.MYSQL_DATABASE,
   process.env.MYSQL_USER,
