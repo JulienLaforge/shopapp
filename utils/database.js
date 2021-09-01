9af2694c11e09cddc65ef16a3e394eeba742e05f
Julien <julien.a.laforge@gmail.com>
2021-09-01 18:01:17 +0200

    change config

--- utils/database.js
+++ utils/database.js
@@ -5,8 +5,7 @@ const sequelize = new Sequelize(
   process.env.MYSQL_USER,
   process.env.MYSQL_PASSWORD,
   { 
-    dialect: 'mysql',
-    host: 'localhost'
+    dialect: 'mysql'
   });
 
 module.exports = sequelize;
